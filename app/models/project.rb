bcfef38f1d484e613131b5439e07672c8a1b6658
Cesar Lazaro <cesar.lazarop@gmail.com>
2018-05-27 17:38:07 +0200

    Create Project User association

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,18 +4,29 @@ class Project < ActiveRecord::Base
   validates_uniqueness_of :title
   validate :free_plan_can_only_have_one_project
 
+  has_many :user_projects
+  has_many :projects, through: :user_projects
+
   def free_plan_can_only_have_one_project
     if self.new_record? && (tenant.projects.count > 0) && (tenant.plan == 'free')
       errors.add(:base, "Free plans cannot have more than one project")
     end
   end
 
-  def self.by_plan_and_tenant(tenant_id)
+  def self.by_user_plan_and_tenant(tenant_id, user)
     tenant = Tenant.find(tenant_id)
     if tenant.plan == 'premium'
-      tenant.projects
+      if user.is_admin?
+        tenant.projects
+      else
+        user.projects.where(tenant_id: tenant.id)
+      end
     else
-      tenant.projects.order(:id).limit(1)
+      if user.is_admin?
+        tenant.projects.order(:id).limit(1)
+      else
+        user.projects.where(tenant_id: tenant.id).order(:id).limit(1)
+      end
     end
   end
 
